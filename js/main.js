6d7c65345d9bbbef4bab878536298759fabdb4ec
Ssenkull <bogdan.zbarazhskiy@gmail.com>
2022-03-26 08:30:40 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function(){
+    $('.slider__box').slick({
+        prevArrow:'<img src="images/arrow-left.svg" alt="photo" class="slider__arrow slider__arrow-left">',
+        nextArrow:'<img src="images/arrow-right.svg" alt="photo" class="slider__arrow slider__arrow-right">',
+        responsive: [
+            {
+                breakpoint: 481,
+                settings:{
+                arrows: false
+            }
+            }
+        ]
+    });
+    $('.menu-btn').on('click', function(){
+        $('.menu__list').toggleClass('active');
+    });
+});
